9bcfde14882f00d9e8c36e716d81338d6dc2eb17
isaacmasterman <142137796+isaacmasterman@users.noreply.github.com>
2024-01-03 22:36:07 +1100

    set up all routes

---
+++ routes/api/friendRoutes.js
@@ -0,0 +1,12 @@
+const router = require('express').Router();
+const { 
+    addFriend, 
+    removeFriend,
+} = require('../../controllers/friendController');
+
+router
+    .route('/:userId/friends/:friendId')
+    .post(addFriend)
+    .delete(removeFriend);
+
+module.exports = router;
---
+++ routes/api/userRoutes.js
@@ -0,0 +1,18 @@
+const router = require('express').Router();
+const { 
+    getAllUsers,
+    getSingleUser, 
+    createUser, 
+    updateUser, 
+    deleteUser,
+} = require('../../controllers/userController');
+
+router.route('/').get(getAllUsers).post(createUser);
+
+router
+    .route('/:id')
+    .get(getSingleUser)
+    .put(updateUser)
+    .delete(deleteUser);
+
+module.exports = router;
