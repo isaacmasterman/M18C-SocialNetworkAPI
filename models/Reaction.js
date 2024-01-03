df1651432db4c5eb9562b97fe1c250f249f805d9
isaacmasterman <142137796+isaacmasterman@users.noreply.github.com>
2024-01-03 21:55:41 +1100

    created models

---
+++ models/Reaction.js
@@ -0,0 +1,28 @@
+const { Schema } = require('mongoose');
+
+const reactionSchema = new Schema({
+    reactionId: {
+      type: mongoose.Schema.Types.ObjectId,
+      default: () => new mongoose.Types.ObjectId()
+    },
+    reactionBody: {
+      type: String,
+      required: true,
+      maxlength: 280
+    },
+    username: {
+      type: String,
+      required: true
+    },
+    createdAt: {
+      type: Date,
+      default: Date.now,
+      get: timestamp => dateFormat(timestamp)
+    }
+  }, {
+    toJSON: {
+      getters: true
+    }
+});
+  
+module.exports = reactionSchema;
